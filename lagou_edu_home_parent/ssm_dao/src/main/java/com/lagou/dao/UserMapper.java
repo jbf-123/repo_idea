676f2afd6e4176f701abdf502ec8135f188269ce
jbf <1635926868@qq.com>
2023-04-23 13:38:49 +0800

    update UserMapper

--- lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -33,8 +33,11 @@ public interface UserMapper {
     public List<Resource> findResourceByRoleId(List<Integer> ids);
     
     public List<Resource> findResourceByRoleId2(List<Integer> ids);
-    
-  
+
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
     
     
 
